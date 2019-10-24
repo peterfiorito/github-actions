edc26f35f0888d16fab425f3d129fe1e330189c0
Peter <pedrojfiorito@gmail.com>
2019-10-24 13:28:42 +0100

    Update entrypoint
    After github actions breaking changes in their new release.

--- commit_changes/entrypoint.sh
+++ commit_changes/entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 set -e
 
+git_setup ( ) {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+    git config user.name "$GITHUB_ACTOR"
+}
+
 if [ -z "$(git status --porcelain)" ]; then
-    echo "no changes"
+    echo "Working tree clean. Nothing to commit."
 else 
-    sh -c "git config --global user.name 'FrontEnd Helper' \
-      && git config --global user.email 'email@domain.co.uk' \
-      && git add -A && git commit -m '$*' --allow-empty \
-      && git push -u origin HEAD"
+    git_setup
+
+    git checkout "$(echo $GITHUB_REF | sed -E 's|refs/[a-zA-Z]+/||')"
+
+    git add .
+
+    git commit -m "Commit bundles"
+
+    git push --set-upstream origin "$(echo $GITHUB_REF | sed -E 's|refs/[a-zA-Z]+/||')"
 fi
